b6d100d26e4edd76c56ef2c9d007ad12f7231987
Ada Okwuchi <okwuchiadanma@gmail.com>
2021-09-03 07:08:10 +0100

    Initial Commit

---
+++ pondsec/src/Components/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import logo from '../Images/logo.png'
+import { Link } from 'react-router-dom'
+import Button from './Button'
+import '../Styles/header.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="app__logo">
+                <img src={logo} alt="pondsec logo" />
+            </div>
+            <nav className="nav-bar">
+                <ul className="nav-links">
+                    <li>
+                        <Link to="/home" className="nav-link">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/home" className="nav-link">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/home" className="nav-link">Features</Link>
+                    </li>
+                </ul>
+            </nav>
+            
+            <div className="header-btn">
+                <Link to="/login" className="nav-link">Login</Link>
+                    <Button className="register-btn"> Create an account </Button>
+
+                </div>
+            
+
+        </header>
+    )
+}
+
+export default Header
